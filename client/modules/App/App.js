2042e85151a2f0b7eca681153fe98e3c41bde96b
georgehardy <george.hardy@gmail.com>
2018-11-17 01:50:55 +1300

    remove boilerplate

--- client/modules/App/App.js
+++ client/modules/App/App.js
@@ -1,18 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-
-// Import Style
-import styles from './App.css';
-
-// Import Components
 import Helmet from 'react-helmet';
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
 
-// Import Actions
-import { toggleAddPost } from './AppActions';
-import { switchLanguage } from '../../modules/Intl/IntlActions';
+import styles from './App.css';
 
 let DevTools;
 if (process.env.NODE_ENV === 'development') {
@@ -20,7 +10,7 @@ if (process.env.NODE_ENV === 'development') {
   DevTools = require('./components/DevTools').default;
 }
 
-export class App extends Component {
+export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = { isMounted: false };
@@ -30,18 +20,13 @@ export class App extends Component {
     this.setState({isMounted: true}); // eslint-disable-line
   }
 
-  toggleAddPostSection = () => {
-    this.props.dispatch(toggleAddPost());
-  };
-
   render() {
     return (
       <div>
         {this.state.isMounted && !window.devToolsExtension && process.env.NODE_ENV === 'development' && <DevTools />}
         <div>
           <Helmet
-            title="MERN Starter - Blog App"
-            titleTemplate="%s - Blog App"
+            title="Takiwa Form"
             meta={[
               { charset: 'utf-8' },
               {
@@ -54,15 +39,9 @@ export class App extends Component {
               },
             ]}
           />
-          <Header
-            switchLanguage={lang => this.props.dispatch(switchLanguage(lang))}
-            intl={this.props.intl}
-            toggleAddPost={this.toggleAddPostSection}
-          />
           <div className={styles.container}>
             {this.props.children}
           </div>
-          <Footer />
         </div>
       </div>
     );
@@ -71,15 +50,4 @@ export class App extends Component {
 
 App.propTypes = {
   children: PropTypes.object.isRequired,
-  dispatch: PropTypes.func.isRequired,
-  intl: PropTypes.object.isRequired,
 };
-
-// Retrieve data from store as props
-function mapStateToProps(store) {
-  return {
-    intl: store.intl,
-  };
-}
-
-export default connect(mapStateToProps)(App);
