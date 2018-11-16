e4eeff6af575b75ec5c5d75b5f6f7fe51fcbb543
georgehardy <george.hardy@gmail.com>
2018-11-17 12:22:53 +1300

    reducer combiner

--- client/reducers.js
+++ client/reducers.js
@@ -4,13 +4,9 @@
 import { combineReducers } from 'redux';
 
 // Import Reducers
-import app from './modules/App/AppReducer';
-import posts from './modules/Post/PostReducer';
-import intl from './modules/Intl/IntlReducer';
+import products from './modules/Products/ProductReducer';
 
 // Combine all reducers into one root reducer
 export default combineReducers({
-  app,
-  posts,
-  intl,
+  products,
 });
