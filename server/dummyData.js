2d8ff46a31768a39160b6f24d83ea72164c55748
georgehardy <george.hardy@gmail.com>
2018-11-17 23:37:26 +1300

    comment

--- server/dummyData.js
+++ server/dummyData.js
@@ -6,6 +6,8 @@ export default function () {
       return;
     }
 
+    // This data used for pre-populating app as well as for tests. Suggest seperating the two if you wish to make changes.
+    //
     const product1 = new Product({ name: 'Animals', cuid: 'cikqgkv4q01ck7453ualdn3hd', items: ['Axalotl', 'Cat', 'Dog', 'Hedgehog', 'Wombat'] });
     const product2 = new Product({ name: 'Beers', cuid: 'cikqgkv4q01ck7453ualdn3hf', items: ['APA'] });
     const product3 = new Product({ name: 'Fruit', cuid: 'cikqgkv4q01ck7453ualdn3he', items: ['Apple', 'Orange', 'Kiwifruit', 'Watermelon'] });
