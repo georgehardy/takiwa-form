5332080c7a3faf0e8142bd011dbef7f2a34794d9
georgehardy <george.hardy@gmail.com>
2018-11-17 20:47:01 +1300

    tidyup

--- client/modules/Products/pages/ProductListPage/ProductListPage.js
+++ client/modules/Products/pages/ProductListPage/ProductListPage.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Link } from 'react-router';
 
 import { fetchProducts, deleteProductRequest } from '../../ProductActions';
 import { getProducts } from '../../ProductReducer';
@@ -20,20 +19,7 @@ class ProductListPage extends Component {
   };
 
   render() {
-    // return (
-    //   <div>
-    //   <Link to="/edit">New</Link>
-    //   <p>h!!i</p>
-    //   {this.props.products.map(item => <Link to={`/edit/${item.cuid}`}><p>{item.name}</p></Link>)}
-    //   </div>
-    //   // <div>
-    //   //   <ProductCreateWidget addProduct={this.handleAddProduct} showAddProduct={this.props.showAddProduct} />
-    //   //   <ProductList handleDeleteProduct={this.handleDeleteProduct} products={this.props.products} />
-    //   // </div>
-    // );
-    return (
-      <ProductList products={this.props.products} handleDelete={this.handleDeleteProduct} />
-    );
+    return <ProductList products={this.props.products} handleDelete={this.handleDeleteProduct} />;
   }
 }
 
